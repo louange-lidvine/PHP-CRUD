e9a58cd9e6ab1d8ade29f12ff575cb53b4ef7c7b
louange-lidvine <lidvinelouange@gmail.com>
2023-05-01 16:02:26 +0200

    added styles

--- updateform.php
+++ updateform.php
@@ -11,39 +11,41 @@
         $_SESSION['id']=$id;
         ?>
     <!-- <div class="container"> -->
+       <fieldset>
+    <div class="container">
         <h2>Signup Form</h2>
-        <form action="update.php" method="post">
-       <input type='hidden' name="id" id="id" value="<?php echo  $id;  ?>"/>
-            <!-- <div class="form-group"> -->
-                <label for="first_name">First Name:</label>
-                <input type="text" id="first_name" name="fname" required>
-            <!-- </div> -->
+        <form action="create.php" method="post">
+            <div class="form-group">
+                <label for="first_name">First Name:</label><br>
+                <input type="text" id="first_name" name="fname" required><br>
+            </div>
 
-            <!-- <div class="form-group"> -->
-                <label for="last_name">Last Name:</label>
-                <input type="text" id="last_name" name="lname" required>
-            <!-- </div> -->
+            <div class="form-group">
+                <label for="last_name">Last Name:</label><br>
+                <input type="text" id="last_name" name="lname" required><br>
+            </div>
 
-            <!-- <div class="form-group"> -->
-                <label for="email">Email:</label>
-                <input type="email" id="email" name="email" required>
-            <!-- </div> -->
+            <div class="form-group">
+                <label for="email">Email:</label><br>
+                <input type="email" id="email" name="email" required><br>
+            </div>
 
-            <!-- <div class="form-group"> -->
-                <label for="password">Password:</label>
-                <input type="password" id="password" name="password" required>
-            <!-- </div> -->
-            <!-- <div class="form-group"> -->
-                <label for="gender">Gender:</label>
+            <div class="form-group">
+                <label for="password">Password:</label><br>
+                <input type="password" id="password" name="password" required><br>
+            </div>
+            <div class="form-group">
+                <label for="gender">Gender:</label><br>
                 <input type="radio" id="gender" name="GENDER" value="male" >
                 <label for="male">Male</label>
                 <input type="radio" id="gender" name="GENDER" value="female">
-                <label for="female">Female</label>
-            <!-- </div> -->
+                <label for="female">Female</label><br>
+            </div>
 
-            <!-- <div class="form-group"> -->
-                <input type="submit" value="Update" name="submit">
-            <!-- </div> -->
+            <div class="form-group">
+                <input type="submit" value="Submit" name="submit" class="submit">
+            </div>
+			</fieldset>
         </form>
     </div>
 </body>
